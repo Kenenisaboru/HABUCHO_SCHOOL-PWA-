b7e69aee756ad936b6d7c0f9e11e48b500a4d9ab
Kenenisaboru <kenenisaboru998@gmail.com>
2026-06-23 06:13:41 -0700

    Refactor and update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -15,6 +15,9 @@ const { Pool } = pg;
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  max: 20, // Max clients in the pool
+  idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
+  connectionTimeoutMillis: 2000, // Error out if connection takes longer than 2 seconds
 });
 
 /**
