fe5500b2a52ec3adf17f82818e43e5c46f5e8be6
Kenenisaboru <kenenisaboru998@gmail.com>
2026-06-23 06:13:44 -0700

    Refactor and update contactRoutes.js

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -11,10 +11,12 @@ import {
   deleteMessage,
 } from "../controllers/contactController.js";
 import { authenticateUser, authorizeRoles } from "../middleware/auth.js";
+import { contactLimiter } from "../middleware/rateLimiter.js";
+import { validateContact } from "../middleware/validate.js";
 
 const router = Router();
 
-router.post("/", authenticateUser, submitMessage);
+router.post("/", authenticateUser, contactLimiter, validateContact, submitMessage);
 router.get("/", authenticateUser, authorizeRoles("admin"), getMessages);
 router.delete("/:id", authenticateUser, authorizeRoles("admin"), deleteMessage);
 
