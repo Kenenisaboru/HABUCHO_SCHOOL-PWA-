94072aaaaf4e9536b02e17b9b1b88c8dfaa21e70
Kenenisaboru <kenenisaboru998@gmail.com>
2026-08-18 07:25:47 -0700

    Update main.jsx entry point

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,24 +3,27 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import App from "./App.jsx";
+import ErrorBoundary from "./components/ErrorBoundary.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import OfflineBanner from "./components/OfflineBanner.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <OfflineBanner />
-        <App />
-        <Toaster
-          position="top-right"
-          toastOptions={{
-            duration: 3000,
-            style: { borderRadius: "10px", background: "#333", color: "#fff" },
-          }}
-        />
-      </ThemeProvider>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <ThemeProvider>
+          <OfflineBanner />
+          <App />
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              duration: 3000,
+              style: { borderRadius: "10px", background: "#333", color: "#fff" },
+            }}
+          />
+        </ThemeProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
   </StrictMode>
 );
